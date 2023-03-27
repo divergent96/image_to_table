c27f4ce6b128265c7673b27d260796c2943e0a4b
divergent96 <susheelpatel.96@gmail.com>
2023-03-27 19:11:26 -0400

    Added EDA and model testing. TODO modules

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,5 @@
 import sys
-import logging
-import logger # Improting custom logger
+from src.logger import logging
 
 def error_message_details(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info() # Ignore first 2 outputs as 3rd capturs details
