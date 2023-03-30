694dba7d4676390e22ba59e47c4d5924e58e36d7
divergent96 <susheelpatel.96@gmail.com>
2023-03-29 23:10:06 -0400

    Created data ingestion and transform components

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -3,3 +3,39 @@ Data transformation component
 
 Stores all required functions and code for transformation and cleaning the data
 '''
+
+import sys
+from dataclasses import dataclass
+
+# Standard libs
+import numpy as np
+import pandas as pd
+
+# Pipeline stuff
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+
+# Data scaling and imputation
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.impute import SimpleImputer
+
+# Import custom components
+from src.exception import CustomException
+from src.logger import logging
+import os
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join('artifacts','preprocessor.pk1')
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+    
+    def get_data_transformer_object(self):
+        try:
+            num_columns = ['writing_score','reading_score']
+        except:
+            pass
+
+
+
