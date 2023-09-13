966892d33cdf8083afaa0770d0a98a2be09edd78
divergent96 <susheelpatel.96@gmail.com>
2023-09-13 19:41:18 -0400

    Sync across new pc

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -4,8 +4,10 @@ Data transformation component
 Stores all required functions and code for transformation and cleaning the data
 '''
 
+# System and native stuff
 import sys
 from dataclasses import dataclass
+import os
 
 # Standard libs
 import numpy as np
@@ -22,7 +24,8 @@ from sklearn.impute import SimpleImputer
 # Import custom components
 from src.exception import CustomException
 from src.logger import logging
-import os
+from src.utils import save_object
+
 @dataclass
 class DataTransformationConfig:
     preprocessor_obj_file_path = os.path.join('artifacts','preprocessor.pk1')
@@ -33,9 +36,90 @@ class DataTransformation:
     
     def get_data_transformer_object(self):
         try:
+            logging.info("Preprocessor configuration Started")
+
             num_columns = ['writing_score','reading_score']
-        except:
-            pass
+            cat_columns = ["gender", "race_ethnicity", "parental_level_of_education", "lunch", "test_preparation_course",]
+
+            num_pipeline = Pipeline(
+                steps = [
+                ("median_imputer", SimpleImputer(strategy="median")),
+                ("std_scaler",StandardScaler())
+                ]
+            )
+
+            cat_pipeline = Pipeline(
+                steps=[
+                ("freq_imputer",SimpleImputer("most_frequent")),
+                ("one_hot_encoder",OneHotEncoder()),
+                ("scaler",StandardScaler(with_mean=False))# Unsure how this helps
+                ]
+            )
+
+            logging.info(f"Numerical columns considered: {num_columns}")
+            logging.info(f"Categorical columns considered: {cat_columns}")
+            
+            preprocessor = ColumnTransformer(
+                [
+                ("num_pipe", num_pipeline, num_columns),
+                ("cat_pipe", cat_pipeline, cat_columns)
+                
+                ]
+            )
+
+            logging.info("Preprocessor configuration completed")
+        except Exception as e:
+            raise CustomException(e, sys)
+
+def transform_data(self, train_path, test_path):
+
+    try:
+        logging.info("Data transformation started")
+        
+        train_df = pd.read_csv(train_path)
+        test_df = pd.read_csv(test_path)
+        logging.info("Imported train and test sets")
+
+        preprocessing_obj = self.get_data_transformer_object()
+
+        target_col_name = "math_score"
+        num_columns = ['writing_score','reading_score']
+
+        input_feat_train_df = train_df.drop(columns = [target_col_name])
+        target_feat_train_df = train_df.loc[:,target_col_name]
+
+        input_feat_test_df = test_df.drop(columns = [target_col_name])
+        target_feat_test_df = test_df.loc[:,target_col_name]
 
 
+        logging.info("Start Preprocessing on training and test sets")
+
+        input_feat_train_arr = preprocessing_obj.fit_transform(input_feat_train_df)
+        input_feat_test_arr = preprocessing_obj.transform(input_feat_test_df)
+        
+
+        train_arr = np.c_[input_feat_train_arr, target_feat_train_df.to_numpy()]
+        test_arr = np.c_[input_feat_test_arr, target_feat_test_df.to_numpy()]
+        
+        logging.info("END Preprocessing train and test sets.")
+
+        logging.info("Saving preprocessed objects")
+
+        save_object(
+            file_path = self.data_transformation_config.preprocessor_obj_file_path,
+            obj = preprocessing_obj
+        )
+
+        logging.info("END preprocessing. Preprocessor has been saved to pickle.")
+        
+        return(
+            train_arr, 
+            test_arr,
+            self.data_transformation_config.preprocessor_obj_file_path,
+        )
+    
+    except Exception as e:
+        CustomException(e, sys)
+
+        
 
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,34 @@
 '''
 Custom utilities that support the components required.
 '''
+
+# System and native stuff
+import sys
+from dataclasses import dataclass
+import os
+
+# Standard libs
+import numpy as np
+import pandas as pd
+
+# Storage libs
+import dill
+import pickle
+
+# Import custom components
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import save_object
+
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path,"wb") as file_obj:
+            pickle.dump(obj, file_obj)
+    
+    except Exception as e:
+        raise CustomException(e, sys)
+    
